e01b2de7d0966ae2acb58bbdb33d56a0808f3c4c
rayor <ruben.aranda.7e5@itb.cat>
2023-01-03 15:59:08 +0100

    Part2

--- src/components/ListOfUsers/ListOfUsers.jsx
+++ src/components/ListOfUsers/ListOfUsers.jsx
@@ -1,20 +1,31 @@
+import React, { useState } from 'react'
 import { Frame, UnorderedList, ListItem, DataItem, ImageItem, ItemName } from './styles'
 
-const ListOfUsers = ({ UserData }) => {
+const ListOfUsers = ({ UserData, selectedUsers = [] }) => {
+  const [listIndex, setIndex] = useState(0)
+
   return (
     <Frame>
       <h1>List Test</h1>
       <UnorderedList>
-        {UserData.map((user, index) =>
-          <ListItem key={index}>
-            <ImageItem src={user.picture.medium} alt='profilePicture' />
-            <DataItem>
-              <ItemName>{user.name.first} {user.name.last}</ItemName>
-              <p>{user.email}</p>
-            </DataItem>
-          </ListItem>
-        )}
+        {UserData.map((user, index) => {
+          if (index === listIndex) {
+            return [...selectedUsers,
+              <ListItem key={index}>
+                <ImageItem src={user.picture.medium} alt='profilePicture' />
+                <DataItem>
+                  <ItemName>{user.name.first} {user.name.last}</ItemName>
+                  <p>{user.email}</p>
+                </DataItem>
+              </ListItem>
+            ]
+          }
+          return null
+        })}
       </UnorderedList>
+      <button onClick={() => { setIndex(listIndex + 1) }}>
+        next
+      </button>
     </Frame>
   )
 }

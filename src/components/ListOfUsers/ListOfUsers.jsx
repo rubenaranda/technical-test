2cca22027f913b78b5e8779f8255692d6e1ee334
rayor <ruben.aranda.7e5@itb.cat>
2023-01-04 11:11:25 +0100

    Part2 (styles and test needed)

--- src/components/ListOfUsers/ListOfUsers.jsx
+++ src/components/ListOfUsers/ListOfUsers.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
 import { Frame, UnorderedList, ListItem, DataItem, ImageItem, ItemName } from './styles'
 
-const ListOfUsers = ({ UserData, selectedUsers = [] }) => {
+const ListOfUsers = ({ UserData }) => {
   const [listIndex, setIndex] = useState(0)
+  const [users, setUsers] = useState([])
+
+  const tuPutaMadreFunction = () => {
+    if (listIndex < UserData.length) {
+      setIndex(listIndex + 1)
+      setUsers([...users, UserData[listIndex]])
+    } else {
+      setUsers(([]))
+      setIndex(0)
+    }
+  }
 
   return (
     <Frame>
       <h1>List Test</h1>
-      <UnorderedList>
-        {UserData.map((user, index) => {
-          if (index === listIndex) {
-            return [...selectedUsers,
-              <ListItem key={index}>
-                <ImageItem src={user.picture.medium} alt='profilePicture' />
-                <DataItem>
-                  <ItemName>{user.name.first} {user.name.last}</ItemName>
-                  <p>{user.email}</p>
-                </DataItem>
-              </ListItem>
-            ]
-          }
-          return null
-        })}
-      </UnorderedList>
-      <button onClick={() => { setIndex(listIndex + 1) }}>
+      {users.map((user, index) => {
+        return (
+          <UnorderedList key={index}>
+            <ListItem>
+              <ImageItem src={user.picture.medium} />
+              <DataItem>
+                <ItemName>{user.name.first} {user.name.last}</ItemName>
+                <p>{user.email}</p>
+              </DataItem>
+            </ListItem>
+          </UnorderedList>
+        )
+      })}
+      <button onClick={tuPutaMadreFunction}>
         next
       </button>
     </Frame>

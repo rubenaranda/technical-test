0593dabbe9c38a0b4bcda24d212d0859c0951e26
rayor <ruben.aranda.7e5@itb.cat>
2023-01-05 11:18:14 +0100

    Data test id and classname added

--- src/components/ListOfUsers/index.jsx
+++ src/components/ListOfUsers/index.jsx
@@ -21,20 +21,20 @@ const ListOfUsers = () => {
     }
   }
   return (
-    <Frame>
-      <Logo src={TravelportLogo} alt='travelport-logo' />
-      <UnorderedList>
+    <Frame data-testid='usersList-frame' className='usersList-frame'>
+      <Logo src={TravelportLogo} alt='travelport-logo' data-testid='usersList-logo' className='usersList-logo' />
+      <UnorderedList data-testid='usersList-ul' className='usersList-ul'>
         {users.map((user, index) =>
-          <ListItem key={index}>
-            <ImageItem src={user.picture.medium} alt='profilePicture' />
+          <ListItem key={index} data-testid='usersList-li' className='usersList-li'>
+            <ImageItem src={user.picture.medium} alt='profilePicture' data-testid='usersList-userImage' className='usersList-userImage' />
             <DataItem>
-              <ItemName>{user.name.first} {user.name.last}</ItemName>
-              <p>{user.email}</p>
+              <ItemName data-testid='usersList-name' className='usersList-name'>{user.name.first} {user.name.last}</ItemName>
+              <p data-testid='usersList-email' className='usersList-email'>{user.email}</p>
             </DataItem>
           </ListItem>
         )}
       </UnorderedList>
-      <AddButton onClick={addUser}>
+      <AddButton onClick={addUser} data-testid='usersList-button' className='usersList-button'>
         Add User
       </AddButton>
     </Frame>

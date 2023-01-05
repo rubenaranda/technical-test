9edd17043d7dd1ed997ee7d62c618f755647013d
rayor <ruben.aranda.7e5@itb.cat>
2023-01-05 11:20:04 +0100

    Change the underline in styles

--- src/components/ListOfUsers/styles.js
+++ src/components/ListOfUsers/styles.js
@@ -20,15 +20,14 @@ justify-content: center;
 
 export const ListItem = styled.li`
 display: inline-flex;
-border-bottom:1px solid black;
 text-align: center;
-opacity:1;
 `
 
 export const DataItem = styled.div`
 display: inline-block;
 ${withTheme('margin-left', 'sizes.medium')}
 text-align: center;
+border-bottom:1px solid black;
 `
 
 export const ImageItem = styled.img`
